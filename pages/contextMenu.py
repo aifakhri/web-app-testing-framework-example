e5b53a85968e33717dec6e1658aeeaf2f25d7d92
fakhri <fakhri.azhar@proton.me>
2022-08-01 09:05:09 +0700

    Change how to use alert method by turning it into two methods instead of one

--- pages/contextMenu.py
+++ pages/contextMenu.py
@@ -1,9 +1,7 @@
 from selenium.webdriver.common.by import By
-
-
 from .common import CommonOps
 
-from selenium import webdriver
+
 
 class ContextMenu(CommonOps):
 
@@ -19,23 +17,11 @@ class ContextMenu(CommonOps):
         context_menu = self.find(self.CONTEXT_BOX)
         self.context_click(context_menu).perform()
 
-    def check_alert(self):
-        return self.alert()
+    def check_alert_message(self):
+        return self.alert().text
+
+    def accept_alert(self):
+        return self.alert().accept()
 
     def alert_is_accepted(self):
         return self.wait_for(self.PAGE_TITLE).text
-
-if __name__ == "__main__":
-    url = "https://the-internet.herokuapp.com"
-    driver = webdriver.Firefox()
-    driver.get(url)
-    try:
-        context = ContextMenu(driver)
-        context.navigate_to_context_menu_page()
-        context.right_click_context_menu()
-        
-        check_alert = context.check_alert()
-        print(check_alert.text)
-        check_alert.accept()
-    finally:
-        driver.close() 
